3da6c131ff1b72332ba378c8cc775426e974b539
ANDISIWEKUNJU <andisiwekunju@gmail.com>
2023-02-17 03:44:10 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include  <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that print a line with put function
+ * main-entry point
  *
- * return:0(success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-          return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
